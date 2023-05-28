ebd316f9f5cd58378f83c90c446fa74eed0c34f9
Leo Reus <reusoliveiraleo@gmail.com>
2023-05-27 23:27:03 -0600

    make it better

---
+++ .history/react-laravel-app/src/hooks/content/content_20230510175502.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import { useForm } from 'react-hook-form'
+
+const Content = ({ keyNumber,
+    image,
+    name,
+    country,
+    }) => {
+
+    const {
+        register,
+        handleSubmit,
+        watch,
+        formState: { errors },
+    } = useForm();
+
+    const navigate = useNavigate()
+
+    const onSubmit = (data) => {
+        navigate("/scholarship-single", { replace: true, state: {data}})
+
+      };
+
+    return (
+        <div className='right-row'>
+            <div className='content-header'>
+                <img src={image} />
+            </div>
+            <div className='content-text-wrapper'>
+                <h1>{name}</h1>
+                <h4>Location of study:</h4>
+                <h2>{country}</h2>
+            </div>
+            <form onSubmit={handleSubmit(onSubmit)} >
+                    <button type={"radio"} {...register("submit-button")} value={`${keyNumber}`}>More information</button>
+                </form>
+        </div>
+    )
+}
+
+export default Content
